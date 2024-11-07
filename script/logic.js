7f7b183b6312074a78e0ff21638fee78fa5480bf
Hussain Vahora <101166530+Hussain-1303@users.noreply.github.com>
2024-11-07 15:12:32 -0500

    Features Added
    Speed Feature : controls the auto scrolling speed
    Restart : allows user to restart the application.
    
    --Removed the horizontal scroll

--- script/logic.js
+++ script/logic.js
@@ -1,4 +1,8 @@
-const scrollSpeed = 1;
+//Vertical Scrolling ONLY  ✅
+//Three buttons  + or - (to increase/decrease the speed) & then restart  ✅
+//Funcionality to pause when hovered over the div and resumes when taken out.
+
+let scrollSpeed = 1;
 
 function scrollVertically() {
   const vScroll = document.getElementById("vScroll");
@@ -7,11 +11,35 @@ function scrollVertically() {
   requestAnimationFrame(scrollVertically);
 }
 
-function scrollHorizontally() {
-  const hScroll = document.getElementById("hScroll");
-  hScroll.scrollLeft += scrollSpeed;
-  requestAnimationFrame(scrollHorizontally);
-}
+// function scrollHorizontally() {
+//   const hScroll = document.getElementById("hScroll");
+//   hScroll.scrollLeft += scrollSpeed;
+//   requestAnimationFrame(scrollHorizontally);
+// }
 
 setTimeout(scrollVertically, 1200);
-setTimeout(scrollHorizontally, 1200);
+
+let verse = document.querySelector(".container .scroll-section #verse");
+let verseData = [];
+
+async function fetchVerse() {
+  try {
+    const response = await fetch("https://raw.githubusercontent.com/gita/gita/refs/heads/main/data/verse.json");
+    verseData = await response.json();
+    console.log(verseData);
+  } catch (error) {
+    console.error('Error Fetching the data');
+  }
+}
+
+fetchVerse();
+
+//FIX : when negative, the auto-scroll get reversed
+function changeSpeed(n) {
+  scrollSpeed = (scrollSpeed + n);
+}
+
+function verseRestart() {
+  fetchVerse();
+  console.log('Restarted');
+}
